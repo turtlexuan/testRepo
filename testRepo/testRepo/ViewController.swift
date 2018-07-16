50900ff08ff1697ed7491310eea2b23a980c91e2
turtlexuan <turtlexuan@gmail.com>
2018-07-16 22:56:53 +0800

    Add new repo

---
+++ testRepo/testRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testRepo
+//
+//  Created by 劉仲軒 on 2018/7/16.
+//  Copyright © 2018年 劉仲軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
